e43229188a374d4c24826f6e15bcb0f01c94a3e6
J1an <jlan45@outlook.com>
2025-03-26 02:50:37 +0800

    Fix Trojan

--- protocol/trojan/trojan.go
+++ protocol/trojan/trojan.go
@@ -15,23 +15,17 @@ type Trojan struct {
 	Port     int
 	Password string
 	SNI      string
+	Insecure bool
 }
 
 func (t Trojan) Dial(network, addr string) (c net.Conn, err error) {
 	ctx := context.Background()
-	host, port, err := net.SplitHostPort(addr)
-	context.WithValue(ctx, "info", map[string]string{
-		"addr":    addr,
-		"network": network,
-		"host":    host,
-		"port":    port,
-	})
 	return t.DialContext(ctx, network, addr)
 }
 
 func (t Trojan) DialContext(ctx context.Context, network, address string) (net.Conn, error) {
 	config := &tls.Config{
-		InsecureSkipVerify: true,
+		InsecureSkipVerify: t.Insecure,
 		ServerName:         t.SNI,
 	}
 	if t.SNI == "" {
@@ -47,30 +41,64 @@ func (t Trojan) DialContext(ctx context.Context, network, address string) (net.C
 	if err != nil {
 		return nil, err
 	}
-	if ctx.Value("info") == nil {
-		host, port, _ := net.SplitHostPort(address)
-		ctx = context.WithValue(ctx, "info", map[string]string{
-			"addr":    address,
-			"network": network,
-			"host":    host,
-			"port":    port,
-		})
+	host, port, err := net.SplitHostPort(address)
+	if err != nil {
+		return nil, err
 	}
+	//+-----------------------+---------+----------------+---------+----------+
+	//| hex(SHA224(password)) |  CRLF   | Trojan Request |  CRLF   | Payload  |
+	//+-----------------------+---------+----------------+---------+----------+
+	//|          56           | X'0D0A' |    Variable    | X'0D0A' | Variable |
+	//+-----------------------+---------+----------------+---------+----------+
+	passHash := tools.SHA224String(t.Password)
+	buf := bytes.Buffer{}
+	buf.Write([]byte(passHash))
+	buf.Write([]byte{0x0d, 0x0a})
+	//+-----+------+----------+----------+
+	//| CMD | ATYP | DST.ADDR | DST.PORT |
+	//+-----+------+----------+----------+
+	//|  1  |  1   | Variable |    2     |
+	//+-----+------+----------+----------+
+	switch network {
+	case "tcp":
+		buf.Write([]byte{0x01})
+	case "udp":
+		buf.Write([]byte{0x03})
+	}
+	ip := net.ParseIP(host)
+	if ip != nil {
+		if ip.To4() != nil {
+			buf.Write([]byte{0x01})
+			buf.Write(ip.To4())
+		} else {
+			buf.Write([]byte{0x04})
+			buf.Write(ip.To16())
+		}
+	} else {
+		buf.Write([]byte{0x03})
+		buf.Write([]byte{byte(len(host))})
+		buf.Write([]byte(host))
+	}
+	portInt, _ := strconv.Atoi(port)
+	buf.Write([]byte{byte(portInt >> 8), byte(portInt)})
+	buf.Write([]byte{0x0d, 0x0a})
 	sc := TrojanConn{
 		RawConn: rawConn,
 		TlsConn: tlsConn,
-		Ctx:     ctx,
 		Info:    t,
+	}
+	_, err = tlsConn.Write(buf.Bytes())
+	if err != nil {
+		return nil, err
 	}
 	// 发送trojan握手，Trojan没有握手
 	return sc, nil
 }
 
 type TrojanConn struct {
-	RawConn net.Conn        // 原始连接
-	TlsConn net.Conn        // tls连接
-	Ctx     context.Context //上下文里面写链接的addr
-	Info    Trojan          // 代理信息
+	RawConn net.Conn // 原始连接
+	TlsConn net.Conn // tls连接
+	Info    Trojan   // 代理信息
 }
 
 func (t TrojanConn) Read(b []byte) (n int, err error) {
@@ -83,45 +111,7 @@ func (t TrojanConn) Read(b []byte) (n int, err error) {
 
 func (t TrojanConn) Write(b []byte) (n int, err error) {
 
-	//+-----------------------+---------+----------------+---------+----------+
-	//| hex(SHA224(password)) |  CRLF   | Trojan Request |  CRLF   | Payload  |
-	//+-----------------------+---------+----------------+---------+----------+
-	//|          56           | X'0D0A' |    Variable    | X'0D0A' | Variable |
-	//+-----------------------+---------+----------------+---------+----------+
-	passHash := tools.SHA224String(t.Info.Password)
-	buf := bytes.Buffer{}
-	buf.Write([]byte(passHash))
-	buf.Write([]byte{0x0d, 0x0a})
-	//+-----+------+----------+----------+
-	//| CMD | ATYP | DST.ADDR | DST.PORT |
-	//+-----+------+----------+----------+
-	//|  1  |  1   | Variable |    2     |
-	//+-----+------+----------+----------+
-	switch t.Ctx.Value("info").(map[string]string)["network"] {
-	case "tcp":
-		buf.Write([]byte{0x01})
-	case "udp":
-		buf.Write([]byte{0x03})
-	}
-	ip := net.ParseIP(t.Ctx.Value("info").(map[string]string)["host"])
-	if ip != nil {
-		if ip.To4() != nil {
-			buf.Write([]byte{0x01})
-			buf.Write(ip.To4())
-		} else {
-			buf.Write([]byte{0x04})
-			buf.Write(ip.To16())
-		}
-	} else {
-		buf.Write([]byte{0x03})
-		buf.Write([]byte{byte(len(t.Ctx.Value("info").(map[string]string)["host"]))})
-		buf.Write([]byte(t.Ctx.Value("info").(map[string]string)["host"]))
-	}
-	port, _ := strconv.Atoi(t.Ctx.Value("info").(map[string]string)["port"])
-	buf.Write([]byte{byte(port >> 8), byte(port)})
-	buf.Write([]byte{0x0d, 0x0a})
-	buf.Write(b)
-	n, err = t.TlsConn.Write(buf.Bytes())
+	n, err = t.TlsConn.Write(b)
 	if err != nil {
 		return 0, err
 	}
